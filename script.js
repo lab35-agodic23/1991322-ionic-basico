e7abe3f86495c8382d03d5c962a0a14f67189057
DavidQM26 <davidquintanillam01@gmail.com>
2023-08-26 23:12:14 -0600

    Practica 3: Mensaje de Login y Registro

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-var campoTexto=document.getElementById("mitexto1");
+var usuario=document.getElementById("usuario");
+var contraseña=document.getElementById("contraseña");
+var currentDiv = document.getElementById("mensaje");
 
-function mostrarAlerta(){
-    alert(campoTexto.value);
+function usuarioRegistrado(){
+    limpiaCampo();
+    currentDiv.innerHTML="";
+    var usuario=document.getElementById("usuario");
+    var newDiv= document.createElement("div");
+    var newContent = document.createTextNode("Registro con éxito " + usuario.value);
+    newDiv.appendChild(newContent);
+    currentDiv.appendChild(newDiv);
+    limpiarCampos();
 }
 
-function limpiarTexto(){
-    campoTexto.value="";
+function usuarioLogeado(){
+    currentDiv.innerHTML="";
+    var usuario=document.getElementById("usuario");
+    var newDiv= document.createElement("div");
+    var newContent = document.createTextNode("Inicio sesión con éxito " + usuario.value);
+    newDiv.appendChild(newContent);
+    currentDiv.appendChild(newDiv);
+    limpiarCampos();
+}
+
+function limpiarCampos(){
+    usuario.value = "";
+    contraseña.value= "";
+}
+
+function limpiaCampo(){
+    document.getElementById("mensaje").value = "";
 }
